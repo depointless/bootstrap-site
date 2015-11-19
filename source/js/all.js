15e7d321bb7ca125311d70647d5500c3a2cf0830
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-11-18 21:59:02 -0300

    angular files, ruby structure

--- source/js/all.js
+++ source/js/all.js
@@ -1,5 +1,6 @@
-//= require jquery
-//= require angular
-//= require bootstrap
+//=require vendor/angular.min
+//=require vendor/ui-bootstrap-tpls-0.14.3.min
+//=require vendor/angular-animate
+//=require vendor/angular-ui-bootstrap-modal
 //= require underscore
-//= require_tree .
+//= require_tree ./lib
