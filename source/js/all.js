ad724a461bc1eafb7ff300a481cd84f39ca0de45
Sabino Velasquez <depointless@Sabinos-Mac-mini.local>
2015-11-28 22:16:13 -0300

    include angular

--- source/js/all.js
+++ source/js/all.js
@@ -1,6 +1,4 @@
-//=require vendor/angular.min
-//=require vendor/ui-bootstrap-tpls-0.14.3.min
-//=require vendor/angular-animate
+//= require_tree ./vendor/angular
 //=require vendor/angular-ui-bootstrap-modal
 //= require underscore
 //= require_tree ./lib
